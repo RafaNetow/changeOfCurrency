d4c54cb210f3f13eade947dff577fcd9bbb1ad09
Rafael Sequeiros <rafanetow@gmail.com>
2018-05-21 03:43:59 -0600

    Added all calls with fake  response to test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require("express");
+const _ = require("lodash")
 
 let app = express();
 
@@ -6,17 +7,45 @@ let port = process.env.Port || 3000;
 
 let CurrrencyRouter = express.Router();
 
-CurrrencyRouter.route("/Currrency")
+CurrrencyRouter.route("/latest/:currency")
 	.get(function (req,res){
-		let responseJson = {hello: "Hellow Loyica"};
+		let responseJson = {hello: "currency response",
+			testo: req.params.currency,
+			queryParam: req.query.test};
 		res.json(responseJson);
 	});
-app.use("/api", CurrrencyRouter);
 
-app.get("/", function(req, res) {
-	res.send("Welcome to my Api :D");
-});
+CurrrencyRouter.route("/latest/:base-:versus")
+	.get(function (req,res){
+		let responseJson = {hello: "base and versus response",
+			base: req.params.base,
+			versus: req.params.versus,
+			queryParam: req.query.test};
+		res.json(responseJson);
+	});  
+CurrrencyRouter.route("/historical/:base-:versus")
+	.get(function (req,res){
+		if (req.query.start || req.query.end) {
+			let responseJson = {hello: "historial base and versus response",
+				base: req.params.base,
+				versus: req.params.versus,
+				start: req.query.start,
+				end: req.query.end};
+			res.json(responseJson);
+		} else if( req.query.date) {
+			let responseJson = {hello: "historial base and versus response",
+				base: req.params.base,
+				versus: req.params.versus,
+				date: req.query.date};
+			res.json(responseJson);
+		}
+		
+
+	}); 
+app.use("/", CurrrencyRouter);
+
+
 
 app.listen(port, function() {
-	console.log("Gulp gonna help me");
+	console.log("Gulp is cool");
 });
