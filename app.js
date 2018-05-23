44f293ba715e54c85671b42ae4bbb034581f2998
Rafael Sequeiros <rafanetow@gmail.com>
2018-05-23 13:46:32 -0600

    rates  working correctly

--- app.js
+++ app.js
@@ -2,22 +2,43 @@ const express = require("express");
 const request = require("request");
 const _ = require("lodash");
 
+
 let app = express();
 
 let port = process.env.Port || 3000;
 
 let CurrrencyRouter = express.Router();
+const listOfCurrencies = ["GBP","AUD","BRL", "CAD","HNL","DKK","GBP","EUR","USD"].join(",");
 
 CurrrencyRouter.route("/latest/:currencies")
 	.get(function (req,res){
 		const ApiKey = "1c7bac7f6711a3c4247b432cd8acbebb";
 		if(req.params.currencies.indexOf("-") > -1) {
 			const currencies = req.params.currencies.split("-");
-			const url = `http://data.fixer.io/api/latest?access_key=${ApiKey}&base=USD&symbols=GBP,JPY,EUR`;
-			res.json({});
+			const base = currencies[0];
+			const versus = currencies[1];
+			const url = `http://data.fixer.io/api/latest?access_key=${ApiKey}&base=${base}&symbols=${listOfCurrencies}`;
+			request(url,(err,response,body) => {
+				if(err) res.json(err);
+				const queryRate = "rates."+versus;
+				
+			//	console.log("usd"+body.rates.EUR);
+				console.log(JSON.parse(body).rates);
+				const rate =_.get(JSON.parse(body),queryRate);
+				console.log("El body es"+body);
+				const apiResponse = JSON.parse(body);
+				
+				const result = {base : apiResponse.base,
+					date: apiResponse.date,
+					versus: versus,
+					rate: rate
+				};
+				res.json(result);
+			});
+			
 			
 		}else{
-			const listOfCurrencies = ["GBP","AUD","BRL", "CAD","HNL","DKK","GBP","EUR","USD"].join(",");
+		
 			const currency = req.params.currencies;
 			
 			const url = `http://data.fixer.io/api/latest?access_key=${ApiKey}&base=${currency}&symbols=${listOfCurrencies}`;
